9bd760a5ebc254a1a8589359d0b265f2f5aad511
tuananhit68 <nguyentuananh0211.tas@gmail.com>
2026-05-13 15:43:12 +0700

    Chức năng đăng nhập

--- Quản lý đăng nhập/Form1.cs
+++ Quản lý đăng nhập/Form1.cs
@@ -16,5 +16,25 @@ namespace Quản_lý_đăng_nhập
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            string emailChuan = "0001168@st.huce.edu.vn";
+            string mssvChuan = "0001168"; // MSSV của Nguyễn Hà Tuấn Anh
+
+            
+            string inputEmail = textBox1.Text.Trim();
+            string inputPass = textBox2.Text.Trim();
+
+            if (inputEmail == emailChuan && inputPass == mssvChuan)
+            {
+                MessageBox.Show("Đăng nhập thành công", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại", "Thông báo");
+            }
+        }
     }
 }
